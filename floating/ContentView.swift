d895267866206ae0b9a511187321e69e9eea5a12
Alexandru Ţurcanu <pondorasti@gmail.com>
2025-10-22 02:22:20 +0200

    Initial Commit

---
+++ floating/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  floating
+//
+//  Created by Alexandru Turcanu on 10/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ floating/floatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  floatingApp.swift
+//  floating
+//
+//  Created by Alexandru Turcanu on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct floatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
